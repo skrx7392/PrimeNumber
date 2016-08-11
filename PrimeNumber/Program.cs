8c760b38f836d680ea7a7163f3a0a002b81865f8
saikr <skrx7392@gmail.com>
2016-08-10 21:06:30 -0400

    First Commit

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -10,6 +10,48 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
+            MainProg: 
+            Console.WriteLine("Please enter an integer: ");
+            int prime = int.Parse(Console.ReadLine());
+            bool isPrime = CheckPrime(prime);
+            if (isPrime)
+            {
+                Console.WriteLine(prime + " is a Prime Number. ");
+            }
+            else
+            {
+                Console.WriteLine(prime + " is not a Prime Number. ");
+            }
+            Console.WriteLine("Do you wish to check another number? (Y/N)");
+            char nextChar = char.Parse(Console.ReadLine().ToUpper());
+            if (nextChar == 'Y')
+                goto MainProg;
+            Console.WriteLine("Thank you!");
+            System.Threading.Thread.Sleep(5000);
+
+        }
+
+        public static bool CheckPrime(int prime)
+        {
+            if(prime<2)
+            {
+                return false;
+            }
+            else if(prime==2)
+            {
+                return true;
+            }
+            else
+            {
+                for(int i=3; i<=Math.Sqrt(prime); i+=2)
+                {
+                    if(prime%i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
